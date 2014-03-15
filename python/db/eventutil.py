3fc63667261f7553eca64b0928c6ca6834375f13
Bill Liu <liu_bill@live.com>
2014-03-15 17:22:35 -0500

    Adding some files, they contain the skeleton for the database side

---
+++ python/db/eventutil.py
@@ -0,0 +1,16 @@
+from common import *
+
+class EventUtil:
+	
+	@staticmethod
+	create_event():
+		return
+	
+	@staticmethod
+	update_event():
+		return
+		
+	@staticmethod
+	get_event():
+		return
+	
