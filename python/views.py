d03721af00d376bd05646f8fde6239650fa1b306
s4lly <jmercad2@illinois.edu>
2014-03-18 02:26:46 -0500

    updated views to provide index with dummy data, user page can be accessed at /user_page

--- python/views.py
+++ python/views.py
@@ -4,4 +4,43 @@ from python import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html")
+
+	events = [ # dummy array of events
+        {
+            'name': 'EOH 2014',
+			'time': '3/19/2014, 1:45pm',
+			'location': 'engineering campus',
+			'food': 'subway'
+        },
+		{
+            'name': 'CS After Hours',
+			'time': '3/20/2014, 4:30pm',
+			'location': 'Seibel center',
+			'food': 'maize'
+        },
+		{
+            'name': 'Career fair',
+			'time': '3/24/2014, 11:00am',
+			'location': 'illini union',
+			'food': 'various'
+        },
+		{
+            'name': 'reflections projections',
+			'time': '4/10/2014, 12:00pm',
+			'location': 'Seibel center',
+			'food': 'pizza'
+        },
+		{
+            'name': 'Bring your family day',
+			'time': '4/14/2014, 10:00am',
+			'location': 'engineering campus',
+			'food': 'chipotle'
+        }
+
+    ]
+
+	return render_template("index.html", events=events)
+
+@app.route('/user_page')
+def user_page():
+	return render_template("user_page_modify.html", user='sally')
--- run_website.py
+++ run_website.py
@@ -3,4 +3,4 @@
 from python import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
