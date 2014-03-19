2db111e0c60e1e07ca1451bb06ddf904f9ba226c
s4lly <jaime.accounts@gmail.com>
2014-03-19 01:56:54 -0500

    updated views.py to display data from database on homepage in semi-working condition

--- python/views.py
+++ python/views.py
@@ -1,43 +1,32 @@
 from flask import render_template
 from python import app
 
+from python.db.common import *
+from python.db.eventutil import EventUtil
+
 @app.route('/')
 @app.route('/index')
 def index():
 
-	events = [ # dummy array of events
-        {
-            'name': 'EOH 2014',
-			'time': '3/19/2014, 1:45pm',
-			'location': 'engineering campus',
-			'food': 'subway'
-        },
-		{
-            'name': 'CS After Hours',
-			'time': '3/20/2014, 4:30pm',
-			'location': 'Seibel center',
-			'food': 'maize'
-        },
-		{
-            'name': 'Career fair',
-			'time': '3/24/2014, 11:00am',
-			'location': 'illini union',
-			'food': 'various'
-        },
-		{
-            'name': 'reflections projections',
-			'time': '4/10/2014, 12:00pm',
-			'location': 'Seibel center',
-			'food': 'pizza'
-        },
-		{
-            'name': 'Bring your family day',
-			'time': '4/14/2014, 10:00am',
-			'location': 'engineering campus',
-			'food': 'chipotle'
-        }
+	connection = connect_to_db()
+	cursor = connection.cursor()
 
-    ]
+	cursor.execute("SELECT * FROM Event")
+
+	results = cursor.fetchall()
+
+	events = []
+
+	for result in results:
+		event = EventUtil.convert_array_to_obj(result)
+
+		event_body = {}
+		event_body['name'] = event.name
+		event_body['time'] = event.time
+		event_body['location'] = event.location
+		event_body['food'] = event.food
+
+		events.append(event_body)
 
 	return render_template("index.html", events=events)
 
