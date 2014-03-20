173fa222da523b33c8f5836f3c525e0eb55d2318
sham00o <lius94@gmail.com>
2014-03-20 00:50:54 -0500

    Update common.py
    changed db credentials for python anywhere

--- python/db/common.py
+++ python/db/common.py
@@ -5,10 +5,10 @@ def connect_to_db():
 	"""
 	Returns a db connection
 	"""
-	hostname = "engr-cpanel-mysql.engr.illinois.edu"
-	usr_name = "teamtemplate_web"
+	hostname = "mysql.server"
+	usr_name = "billliu1992"
 	usr_password = "cs411project"
-	db_name = "teamtemplate_freefood"
+	db_name = "billliu1992@freefood"
 
 	connection = MySQLdb.connect(host = hostname, user = usr_name, passwd = usr_password, db = db_name)
 	
